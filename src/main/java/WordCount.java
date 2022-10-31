fc5b5b20ae89c6db2178651be96cf6e690ab9d21
iorixxx <iorixxx@yahoo.com>
2022-10-31 15:08:52 +0300

    fix possible OOM error using two different approaches

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -3,74 +3,101 @@ import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.* ;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 import java.util.stream.Stream;
 
 @Command(
-		name = "Word Count",
-		description = "It prints the counts of different things in the file"
+        name = "Word Count",
+        description = "It prints the counts of different things in the file"
 )
 
-public class WordCount implements Runnable{
+public class WordCount implements Runnable {
 
-	@Parameters(index="0", description= "The file to count")
-	public String filename;
+    @Parameters(description = "The file to count ben gazöz çöişüğ")
+    public List<String> filenames;
 
 
-	@Option(names = {"-c", "--countBytes"}, description = "Count the number of Bytes in the file")
-	private Boolean countBytes= false;
+    @Option(names = {"-c", "--countBytes"}, description = "Count the number of Bytes in the file")
+    private Boolean countBytes = false;
 
-	@Option(names= {"-w", "countWords"}, description = "Count the number of Words in the file")
-	private Boolean countWords = false;
+    @Option(names = {"-w", "countWords"}, description = "Count the number of Words in the file")
+    private Boolean countWords = false;
 
-	@Option(names = {"-l", "--countLines"}, description = "Count the number of lines in the file")
-	private Boolean countLines =false;
+    @Option(names = {"-l", "--countLines"}, description = "Count the number of lines in the file")
+    private Boolean countLines = false;
 
-	public static long countBytes(Path path) throws IOException {
-		return Files.size(path);
-	}
+    public static long countBytes(Path path) throws IOException {
+        return Files.size(path);
+    }
 
-	public static long countWords(Path path) throws IOException{
-		String sentence = Files.readString(path);
-		StringTokenizer tokens = new StringTokenizer(sentence);
-		return  tokens.countTokens();
-	}
+    public static long countWordsUsingStreams(Path path) {
+        long lines = 0;
+        try (Stream<String> stream = Files.lines(path)) {
+            lines = stream.map(StringTokenizer::new)
+                    .mapToInt(StringTokenizer::countTokens).sum();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	public static long countLines(Path path) {
-		long lines = 0;
-		try (Stream<String> stream =Files.lines(path) ){
-			lines = stream.count();
+        return lines;
+    }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    public static long countWords(Path path) throws IOException {
+        long result = 0;
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            for (; ; ) {
+                String line = reader.readLine();
+                if (line == null)
+                    break;
 
-		return lines;
+                result = new StringTokenizer(line).countTokens();
+            }
+        }
+        return result;
+    }
 
-	}
+    public static long countLines(Path path) {
+        long lines = 0;
+        try (Stream<String> stream = Files.lines(path)) {
+            lines = stream.count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	public static void main(String []args){
-		new CommandLine(new WordCount()).execute(args);
-	}
-	@Override
-	public void run(){
-		try {
-			//not in a try-resource block because path is not auto-closable
-			Path path = Paths.get(filename);
-			//to print all details if no command is specified
-			if (!countBytes & !countLines & !countWords){
-				System.out.println(countLines(path) + " " +countBytes(path) + " "+ countWords(path) + " " + filename);
+        return lines;
 
-			}else{
-				System.out.println( (countLines ? countLines(path): "" ) +(countWords ? countWords(path) +" ": "" ) + (countBytes ? countBytes(path): "" ) + " " +filename);
-			}
+    }
 
-		} catch (IOException e) {
-			//put in real error messages
-			System.out.println(e.toString());
-		}
-	}
+    public static void main(String[] args) {
+
+        System.out.println("\u20BA");
+        new CommandLine(new WordCount()).execute(args);
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            System.out.println(filenames);
+            //not in a try-resource block because path is not auto-closable
+            Path path = Paths.get(filenames.get(0));
+            //to print all details if no command is specified
+            if (!countBytes & !countLines & !countWords) {
+                System.out.println(countLines(path) + " " + countBytes(path) + " " + countWords(path) + " " + filenames.get(0));
+
+            } else {
+                System.out.println((countLines ? countLines(path) : "") + (countWords ? countWords(path) + " " : "") + (countBytes ? countBytes(path) : "") + " " + filenames.get(0));
+            }
+
+        } catch (IOException e) {
+            //put in real error messages
+            System.out.println(e.toString());
+        }
+    }
 
 }
